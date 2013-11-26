05818ed16d2424f9ff5906d1e8b4fec6617951c8
Keito GIBO <e125762@w118-229.dyn.u-ryukyu.ac.jp>
2013-11-26 18:41:57 +0900

    mozinyuuryoku

---
+++ first/first/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  first
+//
+//  Created by Keito GIBO on 2013/11/25.
+//  Copyright (c) 2013年 Keito GIBO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITextFieldDelegate>
+- (IBAction)changeGreeting:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (copy,nonatomic) NSString *userName;
+@end
---
+++ first/first/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  first
+//
+//  Created by Keito GIBO on 2013/11/25.
+//  Copyright (c) 2013年 Keito GIBO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize userName = _userName;
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    NSString *nameString = self.userName;
+    if ([nameString length] == 0) {
+        nameString = @"Word";
+    }
+    NSString *greeting = [[NSString alloc] initWithFormat:@"%@",nameString];
+    self.label.text = greeting;
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    if (theTextField == self.textField) {
+        [theTextField resignFirstResponder];
+    }
+    return YES;
+}
+@end
