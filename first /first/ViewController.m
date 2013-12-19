4c31049eb8bed4c0a05b1f29b1cb547ae3151c69
AkiraYonaha <e125734@akira-no-MacBook-Pro.local>
2013-12-19 09:24:01 +0900

    clean

--- first /first/ViewController.m
+++ first /first/ViewController.m
@@ -18,52 +18,42 @@
 @synthesize again;
 @synthesize output;
 @synthesize userName = _userName;
-@synthesize hairetu = _hairetu;
-@synthesize countButton;
-@synthesize countButton2;
-@synthesize c;
+@synthesize hairetu = _hairetu;  //入力値を記憶
+@synthesize countButton;         //入力回数
+@synthesize countButton2;        //配列の出力要素指定
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-        self.again.hidden = YES;
-        self.output.hidden = YES;
+    _hairetu = [NSMutableArray arrayWithObjects:nil];
+    self.again.hidden = YES;
+    self.output.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-  //  self.changeGreeting.hidden = NO;
+    //  self.changeGreeting.hidden = NO;
     // Dispose of any resources that can be recreated.
     
 }
 
 - (IBAction)changeGreeting:(id)sender {
     self.output.hidden = NO;
-    //ボタンを押した回数をカウント  countButtonの初めが0だったので変更12/5
-    //self.countButton = self.countButton+1;
     //入力した文字をuserNameに入れる
     self.userName = self.textField.text;
-    //入力1回目のときの動作。エラーを消すために
-    if(self.countButton == 0){
-    _hairetu = [NSMutableArray arrayWithObjects:nil];
-    //[_hairetu insertObject:self.userName atIndex:0];
-    }
-    //配列に値を格納
-    [_hairetu insertObject:self.userName atIndex:self.countButton];
-    //これは意味ないであろう
-    if ([_hairetu[0] length] == 0) {
-        _hairetu[0] = @"Word";
-    }
-    //ボタンを押した回数をカウント12/5
-    self.countButton = self.countButton+1;
+    //配列に要素を追加
+    [_hairetu addObject:self.userName];
+    //ボタンを押した回数をカウント
+    self.countButton = self.countButton +1;
     //入力回数を出力
     self.label.text = [NSString stringWithFormat:@"入力%d回",self.countButton];
 }
 
-    //出力ボタンを押したときの動作
+//出力ボタンを押したときの動作
 - (IBAction)Output:(id)sender {
-    //格納ボタンを非表示にする。 12/15
+    //格納ボタンを非表示にする。
     self.changeGreeting.hidden = YES;
     //出力ボタン1回目に配列をシャッフル
     if (countButton2 == 0) {
@@ -73,42 +63,40 @@
             int n = arc4random_uniform(m) + i;
             [_hairetu exchangeObjectAtIndex:i withObjectAtIndex:n];
         }
-        //配列の最後にendを入れる 12/12
-        [_hairetu insertObject:@"end" atIndex:i];
         self.again.hidden = NO;
     }
-    //配列の中のランダムで選んだ位置の要素をとる
-    NSString *greeting = [[NSString alloc] initWithFormat:@"%@",_hairetu[self.c]];
-    //ランダムで選んだ要素を出力
-    self.label.text = greeting;
-    //12/12 出力している配列の文字列がendでない時のみインクリメントするよう変更
-    if([_hairetu count]-1 != self.c){
-        self.c = self.c+1;
+    
+    if ([_hairetu count] > self.countButton2) {
+        //シャッフル後の配列から、順に要素の値を取得
+        NSString *greeting = [[NSString alloc] initWithFormat:@"%@",_hairetu[self.countButton2]];
+        //取得した要素を出力
+        self.label.text = greeting;
+        //出力している配列の要素が最後でない時のみインクリメント
         self.countButton2 = self.countButton2+1;
+    } else {
+        NSString *greeting = @"end";
+        self.label.text = greeting;
     }
+    
 }
 
-//もう一度くじをひくボタン 12/12
+//もう一度くじをひくボタン
 - (IBAction)again:(id)sender {
-    //変数初期か
-    self.c = 0;
+    //変数初期化
     self.countButton2 = 0;
-    //配列の最後のendを消す
-    [_hairetu removeLastObject];
     
-
     self.again.hidden = YES;
     
     self.label.text = [NSString stringWithFormat:@"もう一回"];
 }
+
 //リセットボタンを押したときの動作
 - (IBAction)restart:(id)sender {
-    _hairetu = [NSMutableArray arrayWithObjects:nil];
-    self.c = 0;
+    [_hairetu removeAllObjects];
     self.countButton2 = 0;
     self.countButton=0;
-    self.label.text = [NSString stringWithFormat:@"リセット"];
-    //出力ボタンを表示する。　12/15
+    self.label.text = [NSString stringWithFormat:@"内容をクリアしました。"];
+    //出力ボタンを表示する。
     self.changeGreeting.hidden = NO;
     self.again.hidden = YES;
     self.output.hidden = YES;
